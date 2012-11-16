25f0a123b703dae6864e8da78e5d66c43cd311c2
Brian Hatfield <bhatfield@brightcove.com>
2012-11-16 14:45:18 -0500

    Update license to Apache 2.0; previous 'all rights reserved' was unintentional / an oversight.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Brightcove, Inc"
 maintainer_email "masterchefs@brightcove.com"
-license          "All rights reserved"
+license          "Apache 2.0"
 description      "Installs/Configures ulimit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
