9df1c34067ee5f83322a2e095e1f6c371b9eeefd
Brian Hatfield <bhatfield@brightcove.com>
2012-12-21 12:11:29 -0500

    Cut a new version including memory limit handling

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "masterchefs@brightcove.com"
 license          "Apache 2.0"
 description      "Installs/Configures ulimit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+name             "ulimit"
+version          "0.1.1"
