9bd1dec9f15b2d378181a70bc8905a03f60bd471
Brian Hatfield <bmhatfield@gmail.com>
2013-10-22 23:33:03 -0400

    Resolves ulimit::domain issue

--- resources/rule.rb
+++ resources/rule.rb
@@ -4,4 +4,4 @@ default_action :create
 attribute :type, :kind_of => [Symbol,String], :required => true
 attribute :item, :kind_of => [Symbol,String], :required => true
 attribute :value, :kind_of => [String,Numeric], :required => true
-attribute :domain, :kind_of => [Chef::Resource::UlimitDomain, String], :required => true
+attribute :domain, :kind_of => [Chef::Resource, String], :required => true
