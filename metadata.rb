87d37def29c51e3bc3c2b0f61921ced4216d45c0
Tim Smith <tsmith@chef.io>
2017-04-19 15:21:12 -0700

    Add debian/ubuntu as supported platforms in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name             "ulimit"
 version          "0.5.0"
 
+supports         'debian'
+supports         'ubuntu'
+
 source_url 'https://github.com/bmhatfield/chef-ulimit' if respond_to?(:source_url)
 issues_url 'https://github.com/bmhatfield/chef-ulimit/issues' if respond_to?(:issues_url)
 chef_version '>= 11' if respond_to?(:chef_version)
