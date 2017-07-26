bc72f45d98e6d778c4c10e88e47d853898317098
Tim Smith <tsmith@chef.io>
2017-07-26 16:58:34 -0700

    Better describe the cookbook
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
+name             'ulimit'
 maintainer       'Brian Hatfield'
 maintainer_email 'bmhatfield@gmail.com'
 license          'Apache-2.0'
-description      'Installs/Configures ulimit'
+description      'Resources for manaing ulimits'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-name             'ulimit'
 version          '0.5.0'
 
 supports         'debian'
