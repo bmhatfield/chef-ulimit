febb07717d0612691df66e1a10b070bb9f901a5c
Jamie Zaharopoulos <jzaharopoulos@vigilantglobal.com>
2018-02-01 14:00:01 -0500

    Conversion to new style custom resource

--- resources/domain.rb
+++ resources/domain.rb
@@ -1,51 +1,69 @@
-class Chef::Resource::UlimitDomain < Chef::Resource
-  attr_reader :subresource_rules
+property :domain, String
+property :domain_name, String, name_property: true
+property :filename, String
 
-  def initialize(*args)
-    @subresource_rules = []
-    super
+load_current_value do
+  new_resource.filename new_resource.name unless new_resource.filename
+  new_resource.filename "#{new_resource.filename}.conf"
+
+  new_resource.subresource_rules.map! do |name, block|
+    urule = Chef::Resource::UlimitRule.new("ulimit_rule[#{new_resource.name}:#{name}]", nil)
+    urule.domain new_resource
+    urule.action :nothing
+    urule.instance_eval(&block)
+    unless name
+      urule.name "ulimit_rule[#{new_resource.name}:#{urule.item}-#{urule.type}-#{urule.value}]"
+    end
+    urule
+  end
+end
+
+attr_reader :subresource_rules
+
+def initialize(*args)
+  @subresource_rules = []
+  super
+end
+
+def rule(name = nil, &block)
+  @subresource_rules << [name, block]
+end
+
+default_action :create
+
+action :create do
+  seq = 0
+  new_resource.subresource_rules.map do |sub_resource|
+    sub_resource.run_context = new_resource.run_context
+    sub_resource.run_action(:create)
+  end
+  new_resource.subresource_rules.each do |block|
+    myname = block[0]
+    code = block[1]
+    # The resource used to be named after itself.  Instead now we'll just generate a generic name
+    # Obviously it would be nicer to jump inside ulimit_rule and rename it, however that's
+    # actually tricky to do while maintaining compatability
+    if myname.nil?
+      myname = "rule-#{seq}"
+      seq += 1
+    end
+    ulimit_rule "#{new_resource.name}:#{myname}" do
+      domain new_resource
+      instance_eval &code
+    end
   end
 
-  default_action :create
-
-  property :domain, kind_of: [Chef::Resource::UlimitDomain, String]
-  property :domain_name, kind_of: String, name_property: true
-  property :filename, kind_of: String
-
-  def rule(name = nil, &block)
-    @subresource_rules << [name, block]
+  new_resource.filename new_resource.name unless new_resource.filename
+  new_resource.filename "#{new_resource.filename}.conf"
+  template ::File.join(node['ulimit']['security_limits_directory'], new_resource.filename) do
+    source 'domain.erb'
+    cookbook 'ulimit'
+    variables domain: new_resource.domain_name
   end
+end
 
-  action :create do
-    seq = 0
-    new_resource.subresource_rules.each do |block|
-      myname = block[0]
-      code = block[1]
-      # The resource used to be named after itself.  Instead now we'll just generate a generic name
-      # Obviously it would be nicer to jump inside ulimit_rule and rename it, however that's
-      # actually tricky to do while maintaining compatability
-      if myname.nil?
-        myname = "rule-#{seq}"
-        seq += 1
-      end
-      ulimit_rule "#{new_resource.name}:#{myname}" do
-        domain new_resource
-        instance_eval &code
-      end
-    end
-
-    new_resource.filename new_resource.name unless new_resource.filename
-    new_resource.filename "#{new_resource.filename}.conf"
-    template ::File.join(node['ulimit']['security_limits_directory'], new_resource.filename) do
-      source 'domain.erb'
-      cookbook 'ulimit'
-      variables domain: new_resource.domain_name
-    end
-  end
-
-  action :delete do
-    file ::File.join(node['ulimit']['security_limits_directory'], new_resource.filename) do
-      action :delete
-    end
+action :delete do
+  file ::File.join(node['ulimit']['security_limits_directory'], new_resource.filename) do
+    action :delete
   end
 end
--- resources/rule.rb
+++ resources/rule.rb
@@ -1,21 +1,25 @@
-class Chef::Resource::UlimitRule < Chef::Resource
-  default_action :create
+property :type, [Symbol, String], required: true
+property :item, [Symbol, String], required: true
+property :value, [String, Numeric], required: true
+property :domain, [Chef::Resource, String], required: true
 
-  property :type, kind_of: [Symbol, String], required: true
-  property :item, kind_of: [Symbol, String], required: true
-  property :value, kind_of: [String, Numeric], required: true
-  property :domain, kind_of: [Chef::Resource, String], required: true
+load_current_value do
+  new_resource.domain new_resource.domain.domain_name if new_resource.domain.is_a?(Chef::Resource)
+  node.run_state[:ulimit] ||= Mash.new
+  node.run_state[:ulimit][new_resource.domain] ||= Mash.new
+end
 
-  action :create do
-    new_resource.domain new_resource.domain.domain_name if new_resource.domain.is_a?(Chef::Resource)
-    node.run_state[:ulimit] ||= Mash.new
-    node.run_state[:ulimit][new_resource.domain] ||= Mash.new
-    node.run_state[:ulimit][new_resource.domain][new_resource.item] ||= Mash.new
-    node.run_state[:ulimit][new_resource.domain][new_resource.item][new_resource.type] = new_resource.value
-    puts "Create: #{node.run_state[:ulimit].inspect}"
-  end
+default_action :create
 
-  action :delete do
-    # NOOP
-  end
+action :create do
+  new_resource.domain new_resource.domain.domain_name if new_resource.domain.is_a?(Chef::Resource)
+  node.run_state[:ulimit] ||= Mash.new
+  node.run_state[:ulimit][new_resource.domain] ||= Mash.new
+  node.run_state[:ulimit][new_resource.domain][new_resource.item] ||= Mash.new
+  node.run_state[:ulimit][new_resource.domain][new_resource.item][new_resource.type] = new_resource.value
+  puts "Create: #{node.run_state[:ulimit].inspect}"
+end
+
+action :delete do
+  # NOOP
 end
