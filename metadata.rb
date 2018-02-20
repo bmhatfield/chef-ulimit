ea4c5d2ec50732461d41ce6594a93d24b887b643
Tim Smith <tsmith@chef.io>
2018-02-20 12:31:08 -0800

    Release 1.0.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bmhatfield@gmail.com'
 license          'Apache-2.0'
 description      'Resources for manaing ulimits'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '1.0.0'
 
 %w(amazon centos redhat scientific oracle fedora debian ubuntu).each do |os|
   supports os
